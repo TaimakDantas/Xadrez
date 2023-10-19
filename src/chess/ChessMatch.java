a064406e215cde865167ea11110313f95f21e9cb
Taimak <taimak2010@gmail.com>
2023-10-19 18:08:48 -0300

    Placing pieces on the board

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,9 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.Rei;
+import chess.pieces.Torre;
 
 public class ChessMatch {
 
@@ -9,7 +12,7 @@ public class ChessMatch {
 	public ChessMatch() {
 		
 		board = new Board(8, 8);
-		
+		initialSetup();
 	}
 	
 	public ChessPiece[][] getPieces(){
@@ -24,6 +27,16 @@ public class ChessMatch {
 			
 		}
 		return mat;
+	}
+	
+	private void initialSetup() {
+		
+		board.placePiece(new Torre(board, Color.WHITE), new Position(0,0));
+		board.placePiece(new Torre(board, Color.WHITE), new Position(0,7));
+		board.placePiece(new Torre(board, Color.BLACK), new Position(7,0));
+		board.placePiece(new Torre(board, Color.BLACK), new Position(7,7));
+		board.placePiece(new Rei(board, Color.BLACK), new Position(7,4));
+		board.placePiece(new Rei(board, Color.WHITE), new Position(0,3));
 	}
 	
 }
