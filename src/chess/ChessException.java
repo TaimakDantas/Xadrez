6d59002bfd028eebe425d6ed5e63ee6be0dd49fd
Taimak <taimak2010@gmail.com>
2023-10-20 09:29:14 -0300

    ChessException and CHessPosition

---
+++ src/chess/ChessException.java
@@ -0,0 +1,34 @@
+package chess;
+
+public class ChessException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+
+	public ChessException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public ChessException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ChessException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ChessException(String msg) {
+		super(msg);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ChessException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	
+}
